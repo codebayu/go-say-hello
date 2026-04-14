b3bbb762224e90bfd3ec1a637fc898b545550c11
Bayu Setiawan <bayusetiawanipb@gmail.com>
2026-04-14 15:13:54 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/codebayu/go-say-hello
+
+go 1.26.2
---
+++ say_hello.go
@@ -0,0 +1,7 @@
+package go_say_hello
+
+import "fmt"
+
+func SayHello(name string) string {
+	return fmt.Sprintf("Hello, %s!", name)
+}
